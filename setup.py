8490361720f067cb501903b7a1dd938e6289bdc1
Johannes Wüthrich <johannes@deragent.net>
2020-10-30 13:31:44 +0100

    Bumps version to 0.3.3 for release fix on pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="arxiv-filter",
-    version="0.3.2",
+    version="0.3.3",
     description="Quickly filter the daily arXiv email for your interests.",
     long_description=README,
     long_description_content_type="text/markdown",
