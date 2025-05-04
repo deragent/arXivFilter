ec5c3b473e46e1123d9bd5578777dcd9f23eefa6
Johannes Wüthrich <johannes@deragent.net>
2025-05-04 18:32:35 +0200

    Adds helper functions to string matching
    - Integrating into a definition item.
    - Matching while honouring the exact flag

--- arxiv_filter/config/Definition.py
+++ arxiv_filter/config/Definition.py
@@ -31,6 +31,48 @@ class DefinitionItem:
     def isExact(self) -> bool:
         return self._exact
 
+    def _findAll(self, key, string):
+        """
+        Return list of all indexes of substring `key` in `string`.
+
+        Overlapping matches of `key` are allowed.
+        """
+        idx = string.find(key)
+        while idx != -1:
+            yield idx
+            idx = string.find(key, idx+1)
+
+
+    def findInString(self, string_clean, string_original, mapping):
+        """
+        Find all locations of the `keyClean` in `string_clean`.
+        This functions honours the exact matching if requested!
+
+        Exact matching is implemented by checking characters before and after each match
+        in the original (non-sanitized) string `string_original`.
+        """
+        indexes = self._findAll(self.keyClean, string_clean)
+        if not self.isExact:
+            # If a non-exact match is ok, we can simply return the clean matches
+            return list(indexes)
+        else:
+            exact_indexes = []
+            for idx in indexes:
+                index_before = mapping[idx] - 1
+                index_after = mapping[idx+len(self.keyClean)-1] + 1
+
+                if index_before >=0  and string_original[index_before].isalpha():
+                    # Character before the match is alphabet character
+                    continue
+                if index_after < len(string_original) and string_original[index_after].isalpha():
+                    # Character after the match is alphabet character
+                    continue
+
+                exact_indexes.append(idx)
+
+            return exact_indexes
+
+
 
 class Definition:
 
